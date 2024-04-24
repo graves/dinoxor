5932b43f26e18dd9ff1fddcfa43fe9f57d2962d4
Thomas Gentry <thomas@awfulsec.com>
2024-04-24 01:19:00 -0400

    Move stuff around

--- src/thechinesegovernment.rs
+++ src/thechinesegovernment.rs
@@ -14,7 +14,7 @@ use std::arch::asm;
 /// # Example
 ///
 /// ```
-/// use crate::thechinesegovernment::dinoxor;
+/// use thechinesegovernment::thechinesegovernment::dinoxor;
 /// 
 /// let result = dinoxor(0b11101011, 0b11111111);
 /// assert_eq!(result, 0b10100 )
